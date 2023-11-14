8f2f87acde3c4876d685b8ef694ea621f42e9d58
bya <byambaa.mng@gmail.com>
2023-11-13 18:31:36 -0800

    Ass5: Recycled layout

--- walmart/app/src/main/java/com/example/walmart/Product.kt
+++ walmart/app/src/main/java/com/example/walmart/Product.kt
@@ -1,3 +1,11 @@
 package com.example.walmart
 
-data class Product(val productName: String, val productDescription: String, val cost: Double, val imageId: Int)
+import java.io.Serializable
+
+data class Product(
+    val productName: String,
+    val productDescription: String,
+    val cost: Double,
+    val imageId: Int,
+    val logoId: Int,
+) : Serializable
